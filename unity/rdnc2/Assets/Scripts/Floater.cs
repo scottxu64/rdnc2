da924bd874d8150b4e0366ca726e0cd65ce9a6e6
RX <xurunpei@gmail.com>
2023-01-02 16:59:18 -0500

    floating effect

---
+++ unity/rdnc2/Assets/Scripts/Floater.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    public Rigidbody floaterRigidbody;
+    public float balancedUnderWaterHeight = 1f;     // distance from center point to bottom, when balanced
+    public float buoyancyMultiplier = 3f;           // buoyancy when full underwater body is under water
+
+    private void FixedUpdate()
+    {
+        if (transform.position.y < 0f)
+        {
+            var buoyancy = Mathf.Abs( Mathf.Clamp01(-transform.position.y / balancedUnderWaterHeight) * buoyancyMultiplier * Physics.gravity.y );
+            floaterRigidbody.AddForce(new Vector3(0f, buoyancy, 0f), ForceMode.Acceleration);
+        }
+    }
+}
