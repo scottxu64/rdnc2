161bb49ec4596c83428d32e1ec26a438528b931d
RX <xurunpei@gmail.com>
2023-01-18 09:53:25 -0500

    add cam move controller, switch to android

---
+++ unity/rdnc2/Assets/Scripts/CamController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public Transform target;
+    public float leftLimit = -100f;
+    public float rightLimit = 100f;
+    public float speed = 1f;
+
+    public float totalMovement = 0f;    // to display value in game mode
+
+    private void FixedUpdate()
+    {
+        float input = Input.acceleration.x;
+        if (input == 0) input= Input.GetAxisRaw("Horizontal");
+
+        totalMovement = Mathf.Clamp(totalMovement + input, leftLimit, rightLimit);
+        if (input!=0) Debug.Log("input=" + input + ", totalMovement=" + totalMovement);
+
+        if (totalMovement < rightLimit && totalMovement > leftLimit)
+        {
+            transform.RotateAround(target.position, Vector3.up, input*speed);
+        }
+    }
+}
